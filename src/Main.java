444f667373aca515864c737a7d2bedc346c2f57d
Davi12435 <davipadilha555@gmail.com>
2025-02-21 23:16:48 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World! davi e cagaoooooooo e pao");
+        System.out.println("Hello");
     }
 
 }
